07837fdbc2ca0b4c1c878178fd29d1c18118e6a5
tammenterho <terho.mikko@gmail.com>
2023-09-08 16:52:31 +0300

    week1 done

---
+++ Week1/week1.js
@@ -0,0 +1,26 @@
+const clickButton = document.getElementById("my-button");
+
+clickButton.addEventListener("click", function () {
+  console.log("hello world");
+
+  // https://stackoverflow.com/questions/9529327/change-the-value-of-h1-element-within-a-form-with-javascript
+
+  if (document.getElementById("titleText").innerHTML === "Moi maailma") {
+    document.getElementById("titleText").innerHTML = "hello world";
+  } else if (document.getElementById("titleText").innerHTML === "hello world") {
+    document.getElementById("titleText").innerHTML = "Moi maailma";
+  }
+});
+
+// https://stackoverflow.com/questions/20673959/how-to-add-new-li-to-ul-onclick-with-javascript
+
+const addTextButton = document.getElementById("add-data");
+
+addTextButton.addEventListener("click", function () {
+  const ulList = document.getElementById("my-list");
+  const li = document.createElement("li");
+  const message = document.getElementById("text").value;
+
+  li.appendChild(document.createTextNode(message));
+  ulList.appendChild(li);
+});
