f7766cf123b6ff462a7b0fb00dd518ee48bd0990
tammenterho <terho.mikko@gmail.com>
2023-09-11 18:20:55 +0300

    exercises 1-4

---
+++ Week2/week2.js
@@ -0,0 +1,67 @@
+const submit = document.getElementById("submit-data");
+const form = document.getElementById("my-form");
+const table = document.getElementById("my-table");
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  //https://stackoverflow.com/questions/64483666/insert-form-values-in-a-table-using-javascript
+
+  const newRow = table.insertRow();
+  // const formElements = form.elements;
+
+  const usernameCell = newRow.insertCell();
+  const emailCell = newRow.insertCell();
+  const adminCell = newRow.insertCell();
+
+  let username = document.getElementById("input-username").value;
+  let email = document.getElementById("input-email").value;
+  let admin = document.getElementById("input-admin").checked ? "X" : " ";
+
+  let usernameExists = false;
+
+  for (var i = 1; i < table.rows.length; i++) {
+    //console.log(table.rows[i].cells[0].textContent);
+
+    if (table.rows[i].cells[0].textContent === username) {
+      console.log("löytyy jo");
+      console.log(i);
+      usernameExists = true;
+
+      const modifiedUsername = table.rows[i].cells[0].value;
+      table.rows[i].cells[1].innerHTML =
+        document.getElementById("input-email").value;
+      table.rows[i].cells[2].innerHTML = document.getElementById("input-admin")
+        .checked
+        ? "X"
+        : " ";
+
+      break;
+    }
+  }
+  if (!usernameExists) {
+    usernameCell.innerHTML = username;
+    emailCell.innerHTML = email;
+    adminCell.innerHTML = admin;
+  }
+
+  form.reset();
+});
+
+// https://stackoverflow.com/questions/63335136/how-to-add-a-button-to-clear-all-table-rows
+// https://stackoverflow.com/questions/60804476/how-to-empty-table-in-javascript
+
+const deleteData = document.getElementById("empty-table");
+
+deleteData.addEventListener("click", function () {
+  for (var i = 1; i < table.rows.length; i++) {
+    table.rows[i].innerHTML = "";
+
+    /*
+    table.rows[i].cells[0].innerHTML = "";
+    table.rows[i].cells[1].innerHTML = "";
+    table.rows[i].cells[2].innerHTML = "";
+
+    */
+  }
+});
