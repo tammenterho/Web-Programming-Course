a19a6d22c6eb7a434a3d61ad6aaa9a7ed5be3688
tammenterho <terho.mikko@gmail.com>
2023-09-18 18:37:51 +0300

    employment added

--- Week3/week3.js
+++ Week3/week3.js
@@ -14,6 +14,12 @@ async function getMunicipalityAndPopulation() {
   const pJSON = await ppromise.json();
   console.log(pJSON);
 
+  const employmentUrl =
+    "https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065";
+  const epromise = await fetch(employmentUrl);
+  const eJSON = await epromise.json();
+  console.log(eJSON);
+
   const tbody = document.querySelector("tbody");
 
   for (const code in mJSON.dataset.dimension.Alue.category.index) {
@@ -22,17 +28,23 @@ async function getMunicipalityAndPopulation() {
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
 
     td1.innerText = municipalityName;
 
     const populationCode = pJSON.dataset.dimension.Alue.category.index[code];
-    console.log(populationCode);
+    // console.log(populationCode);
     const population = pJSON.dataset.value[populationCode];
 
     td2.innerText = population;
 
+    const employment = eJSON.dataset.value[populationCode];
+
+    td3.innerText = employment;
+
     tr.appendChild(td1);
     tr.appendChild(td2);
+    tr.appendChild(td3);
     tbody.appendChild(tr);
   }
 }
