83b6437408af8047827e13e2a792c0ed38e119ac
tammenterho <terho.mikko@gmail.com>
2023-09-29 14:25:26 +0300

    shows more shows and empties search

--- Week4/week4.js
+++ Week4/week4.js
@@ -1,4 +1,6 @@
 const form = document.querySelector("form");
+const showContainer = document.querySelector(".show-container");
+
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
 
@@ -10,47 +12,53 @@ form.addEventListener("submit", async function (event) {
     alert("Syötä TV-sarjan nimi ennen hakemista.");
     return;
   }
+  showContainer.innerHTML = "";
 
-  await getTvShow(inputShow);
+  await getTvShows(inputShow);
 });
-async function getTvShow(inputShow) {
+
+async function getTvShows(inputShow) {
   const tvUrl = `https://api.tvmaze.com/search/shows?q=${inputShow}`;
   const tvPromise = await fetch(tvUrl);
   const tvJson = await tvPromise.json();
 
-  const showContainer = document.querySelector(".show-container");
+  if (tvJson.length > 0) {
+    tvJson.forEach((result) => {
+      const show = result.show;
 
-  const showTitle = tvJson[0].show.name;
-  const showSummary = tvJson[0].show.summary;
-  const showImg = tvJson[0].show.image.medium;
-  console.log(JSON.stringify(showTitle));
-  console.log(JSON.stringify(showSummary));
+      const showTitle = show.name;
+      const showSummary = show.summary;
+      const showImg = show.image.medium;
 
-  let divData = document.createElement("div");
-  divData.classList.add("show-data");
+      let divData = document.createElement("div");
+      divData.classList.add("show-data");
 
-  let img = document.createElement("img");
+      let img = document.createElement("img");
 
-  let divInfo = document.createElement("div");
-  divInfo.classList.add("show-info");
+      let divInfo = document.createElement("div");
+      divInfo.classList.add("show-info");
 
-  let h1 = document.createElement("h1");
-  let p = document.createElement("p");
+      let h1 = document.createElement("h1");
+      let p = document.createElement("p");
 
-  h1.innerText = showTitle;
+      h1.innerText = showTitle;
 
-  img.src = showImg;
-  img.alt = showTitle;
+      img.src = showImg;
+      img.alt = showTitle;
 
-  const tempElement = document.createElement("div");
-  tempElement.innerHTML = showSummary;
-  const summaryText = tempElement.textContent;
+      const tempElement = document.createElement("div");
+      tempElement.innerHTML = showSummary;
+      const summaryText = tempElement.textContent;
 
-  p.innerText = summaryText;
+      p.innerText = summaryText;
 
-  divData.appendChild(img);
-  divInfo.appendChild(h1);
-  divInfo.appendChild(p);
-  divData.appendChild(divInfo);
-  showContainer.appendChild(divData);
+      divData.appendChild(img);
+      divInfo.appendChild(h1);
+      divInfo.appendChild(p);
+      divData.appendChild(divInfo);
+      showContainer.appendChild(divData);
+    });
+  } else {
+    showContainer.innerHTML = "<p>Ohjelmia ei löytynyt.</p>";
+  }
 }
